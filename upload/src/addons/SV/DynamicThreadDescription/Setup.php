3a4a510c7d4c863d2fd96278dc38588c4e0dce7f
Xon <tazipper1@hotmail.com>
2021-08-02 12:50:41 +0800

    add-on skeleton

---
+++ upload/src/addons/SV/DynamicThreadDescription/Setup.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SV\DynamicThreadDescription;
+
+use SV\StandardLib\InstallerHelper;
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+/**
+ * Add-on installation, upgrade, and uninstall routines.
+ */
+class Setup extends AbstractSetup
+{
+    use InstallerHelper;
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
+}

51c3e0102f5f5b55d1686503a2c458fad16bb779
Xon <635541+Xon@users.noreply.github.com>
2023-12-09 13:54:56 +0800

    Remove unused dependency on SV/StandardLib

--- upload/src/addons/SV/DynamicThreadDescription/Setup.php
+++ upload/src/addons/SV/DynamicThreadDescription/Setup.php
@@ -2,7 +2,6 @@
 
 namespace SV\DynamicThreadDescription;
 
-use SV\StandardLib\InstallerHelper;
 use XF\AddOn\AbstractSetup;
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
@@ -13,7 +12,6 @@ use XF\AddOn\StepRunnerUpgradeTrait;
  */
 class Setup extends AbstractSetup
 {
-    use InstallerHelper;
     use StepRunnerInstallTrait;
     use StepRunnerUpgradeTrait;
     use StepRunnerUninstallTrait;

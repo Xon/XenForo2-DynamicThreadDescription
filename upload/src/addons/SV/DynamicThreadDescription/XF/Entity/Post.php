a5c9e51a22000dee5943b30df811c68b848d5e56
Xon <635541+Xon@users.noreply.github.com>
2023-12-09 14:54:40 +0800

    Remove intersection observer code

--- upload/src/addons/SV/DynamicThreadDescription/XF/Entity/Post.php
+++ upload/src/addons/SV/DynamicThreadDescription/XF/Entity/Post.php
@@ -18,19 +18,4 @@ class Post extends XFCP_Post
 
         return parent::getLinkToThreadmarkReaderMode($extra);
     }
-/*
-    public function getRedirectLinkForThreadmark($canonical = false, $footer = false)
-    {
-        $thread = $this->Thread;
-
-        if (!$canonical && $thread)
-        {
-            $page = \floor($this->position / \XF::options()->messagesPerPage) + 1;
-
-            return $this->app()->router()->buildLink('threads', $thread, ['page' => $page, 'post' => $this->post_id]) . '#post-' . ($footer ? 'footer-' : '') . $this->post_id;
-        }
-
-        return parent::getRedirectLinkForThreadmark($canonical, $footer);
-    }
-*/
 }
